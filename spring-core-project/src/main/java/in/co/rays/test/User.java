4cd2c66552dddc3ba114c9291e5e17949fc4ff66
ChaitanyaBhatt147 <bhattchaitanya43@gmail.com>
2026-02-02 17:18:47 +0530

    done

---
+++ spring-core-project/src/main/java/in/co/rays/test/User.java
@@ -0,0 +1,31 @@
+package in.co.rays.test;
+
+public class User {
+	private String name;
+	private String address;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public User() {
+	}
+
+	public User(String name, String address) {
+		this.name = name;
+		this.address = address;
+	}
+
+}
